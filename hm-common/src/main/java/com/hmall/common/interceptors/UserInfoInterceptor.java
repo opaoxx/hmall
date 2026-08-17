061919f2b5431749ecb4e84900a731ff4d0f66e7
opaoxx <3485967226@qq.com>
2026-08-17 15:36:38 +0800

    添加网关，过滤器，拦截器，gateway，globalfilter，interceptor

---
+++ hm-common/src/main/java/com/hmall/common/interceptors/UserInfoInterceptor.java
@@ -0,0 +1,28 @@
+package com.hmall.common.interceptors;
+
+import cn.hutool.core.util.StrUtil;
+import com.hmall.common.utils.UserContext;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class UserInfoInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        //获取用户登录信息
+        String userInfo = request.getHeader("userInfo");
+        //判断是否获取成功，如果有，则存入ThreadLocal
+        if(StrUtil.isNotBlank(userInfo)){
+            UserContext.setUser(Long.valueOf(userInfo));
+        }
+        //放行
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        // 清理ThreadLocal
+        UserContext.removeUser();
+    }
+}
