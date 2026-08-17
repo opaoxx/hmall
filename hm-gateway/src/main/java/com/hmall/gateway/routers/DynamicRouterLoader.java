926436bfd07d9fb16cb068ba100f625df912f68a
opaoxx <3485967226@qq.com>
2026-08-17 16:59:15 +0800

    添加共享配置到nacos，拉取共享配置，配置热更新，动态路由相关功能

---
+++ hm-gateway/src/main/java/com/hmall/gateway/routers/DynamicRouterLoader.java
@@ -0,0 +1,69 @@
+package com.hmall.gateway.routers;
+
+import cn.hutool.json.JSONUtil;
+import com.alibaba.cloud.nacos.NacosConfigManager;
+import com.alibaba.nacos.api.config.listener.Listener;
+import com.alibaba.nacos.api.exception.NacosException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.route.RouteDefinition;
+import org.springframework.cloud.gateway.route.RouteDefinitionWriter;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+import javax.annotation.PostConstruct;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.Executor;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class DynamicRouterLoader {
+
+    private final NacosConfigManager nacosConfigManager;
+
+    private final RouteDefinitionWriter routeDefinitionWriter;
+
+    private final String dataId = "gateway-routes.json";
+
+    private final String group = "DEFAULT_GROUP";
+
+    private final Set<String> routeIds = new HashSet<>();
+
+    @PostConstruct
+    public void initRouteConfigListener() throws NacosException {
+        //项目启动时先拉取一次配置（因为配置全都在nacos中，不在本地yaml），并且添加监听器
+        String configInfo = nacosConfigManager.getConfigService().getConfigAndSignListener(dataId, group, 5000, new Listener() {
+            @Override
+            public Executor getExecutor() {
+                return null;
+            }
+
+            @Override
+            public void receiveConfigInfo(String configInfo) {
+                //监听到配置变更，需要去更新路由表
+                updateConfigInfo(configInfo);
+            }
+        });
+        //第一次读取到配置，也需要更新路由表(初始化）
+        updateConfigInfo(configInfo);
+    }
+
+    private void updateConfigInfo(String configInfo) {
+        //解析配置信息，转为RouteDefinition
+        List<RouteDefinition> routeDefinitions = JSONUtil.toList(configInfo, RouteDefinition.class);
+        //删除旧的路由表
+        for (String routeId : routeIds){
+            routeDefinitionWriter.delete(Mono.just(routeId)).subscribe();
+        }
+        routeIds.clear();
+        //更新路由表
+        for (RouteDefinition routeDefinition : routeDefinitions){
+            routeDefinitionWriter.save(Mono.just(routeDefinition)).subscribe();
+            //记录路由id，便于下一次更新时删除旧的路由表
+            routeIds.add(routeDefinition.getId());
+        }
+    }
+}
